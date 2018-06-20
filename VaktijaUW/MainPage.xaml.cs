a7402ff3aad28f6314587db8129ccc4640f7f057
Mithat Hackovic <hackovic@gmail.com>
2018-06-20 13:58:21 +0200

    Dodaj januar
    Dodaj vrijeme za januar
    Ukloni RTC pozive

--- VaktijaUW/MainPage.xaml.cs
+++ VaktijaUW/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace VaktijaUW
@@ -14,11 +16,15 @@ namespace VaktijaUW
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //RTC_DS3231 _clock;
+
         public MainPage()
         {
             InitializeComponent();
             // Ako imate RTC instaliran na Raspberry Pi
-            RealTimeClock.SetTime();
+            //RealTimeClock.SetTime();
+
+            //InitRTC();
 
             CultureInfo.CurrentUICulture = new CultureInfo("bs-Latn-BA");
             Takvim = VaktijaUW.Takvim.KreirajVaktiju();
@@ -26,7 +32,33 @@ namespace VaktijaUW
             Task.Run(PokreniVaktijuAsync);
         }
 
-        public static DateTime Time { get; set; } = DateTime.Now;
+        //private void InitRTC()
+        //{
+        //    _clock = new RTC_DS3231();
+        //    Task.Run(()=> _clock.FindI2CAddressesAsync(Msg));
+        //    DispatcherTimer timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+        //    timer.Tick += (oo, ee) =>
+        //    {
+        //        var t = _clock.ReadTime();
+        //        Time2.Text = t.HasValue ?t.ToString(): "No time";
+        //        Temperature.Text = _clock.ReadTemperature().ToString();
+        //    };
+        //    timer.Start();
+        //}
+
+        //private async Task Msg(Message arg)
+        //{
+        //    await Task.Run(()=> Debug.WriteLine(arg));
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    _clock.WriteTime(DateTime.Now.ToUniversalTime());
+        //    DateTime dt = _clock.ReadTime().Value;
+        //}
+
+
+        //public static DateTime Time { get; set; } = DateTime.Now;
 
         private List<Dan> Takvim { get; }
         public List<Praznik> Praznici { get; }
@@ -44,8 +76,9 @@ namespace VaktijaUW
 
         private async Task PostaviVrijeme()
         {
-            var rtcTime = await RealTimeClock.GetTimeFromDs3231();
-            Time = rtcTime ?? DateTime.Now;
+            //var rtcTime = await RealTimeClock.GetTimeFromDs3231();
+            //Time = rtcTime ?? DateTime.Now;
+            //Time = DateTime.Now;
 
             Danas = (from dan in Takvim
                 where dan.Datum == DateTime.Today
@@ -70,9 +103,9 @@ namespace VaktijaUW
                     if (Danas == null) return;
 
                     SetZora();
-                    SetStyle(Podne, "Podne", Danas.Podne, Danas.Ikindija);
+                    SetStyle(Podne, "Podne ", Danas.Podne, Danas.Ikindija);
                     SetStyle(Ikindija, "Ikindija", Danas.Ikindija, Danas.Aksam);
-                    SetStyle(Aksam, "Akšam", Danas.Aksam, Danas.Jacija);
+                    SetStyle(Aksam, "Akšam ", Danas.Aksam, Danas.Jacija);
                     SetJacija();
                 });
         }
@@ -111,8 +144,8 @@ namespace VaktijaUW
                 Izlazak.StyleProsloVrijeme();
             }
 
-            Zora.SetRed("Zora", Danas.Zora);
-            Izlazak.SetRed("Sabah", Danas.Sabah);
+            Zora.SetRed("Zora  ", Danas.Zora);
+            Izlazak.SetRed("Sabah ", Danas.Sabah);
         }
 
         private void SetJacija()
--- VaktijaUW/RealTimeClock.cs
+++ VaktijaUW/RealTimeClock.cs
@@ -7,6 +7,184 @@ using Windows.Devices.I2c;
 
 namespace VaktijaUW
 {
+
+    public class Message
+    {
+        public Message(string msg)
+        {
+             Msg  = msg;
+        }
+
+        public string Msg { get; }
+        public override string ToString()
+        {
+            return Msg;
+        }
+    }
+
+    public class RTC_DS3231
+    {
+        I2cDevice _device;
+
+        private bool InitComplete { get; set; } = false;
+
+        public RTC_DS3231() : this(0x68)
+        {
+        }
+
+        public RTC_DS3231(int address)
+        {
+            Init(address);
+        }
+
+
+        public async Task<bool> FindI2CAddressesAsync(Func<Message, Task> onMessageReceived)
+        {
+            string aqs = I2cDevice.GetDeviceSelector("I2C1");
+
+            var i2CDevices = await DeviceInformation.FindAllAsync(aqs).AsTask().ConfigureAwait(false);
+            if (i2CDevices.Count == 0)
+            {
+                await onMessageReceived(new Message("bus not found")).ConfigureAwait(false);
+                return false;
+            }
+
+            for (byte slaveAddress = 0x00; slaveAddress <= 0x7F; slaveAddress++)
+            {
+                var settings = new I2cConnectionSettings(slaveAddress)
+                {
+                    BusSpeed = I2cBusSpeed.FastMode,
+                    SharingMode = I2cSharingMode.Shared
+                };
+
+                foreach (var i2CDevice in i2CDevices)
+                {
+                    var i2CDeviceDetails = await I2cDevice.FromIdAsync(i2CDevice.Id, settings).AsTask().ConfigureAwait(false);
+
+                    try
+                    {
+                        var testCommand = new byte[] { 0x00, 0x0 };
+                        i2CDeviceDetails.Read(testCommand);
+
+                        await onMessageReceived(new Message($"SlaveAddress: {settings.SlaveAddress:X} SUCCESS \n"));
+                    }
+                    catch (Exception)
+                    {
+                        await onMessageReceived(new Message($"SlaveAddress: {settings.SlaveAddress:X} FAILED \n"));
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+        private async void Init(int address)
+        {
+            if (InitComplete)
+                return;
+            try
+            {
+                var advancedQuerySyntaxString = I2cDevice.GetDeviceSelector();
+                var controllerDeviceIds = await DeviceInformation.FindAllAsync(advancedQuerySyntaxString);
+                I2cConnectionSettings connectionSettings = new I2cConnectionSettings(address)
+                {
+                    BusSpeed = I2cBusSpeed.StandardMode
+                };
+                _device = await I2cDevice.FromIdAsync(controllerDeviceIds[0].Id, connectionSettings);
+                InitComplete = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
+        }
+
+        public DateTime? ReadTime()
+        {
+            try
+            {
+
+            byte[] readBuffer = new byte[0x13];
+            _device.WriteRead(new byte[] { 0x00 }, readBuffer);
+
+            int seconds = BcdToInt(readBuffer[0]);
+            int minutes = BcdToInt(readBuffer[1]);
+            bool is24HourCock = (readBuffer[2] >> 0x6) != 1;
+            int hours;
+            if (is24HourCock)
+                hours = (readBuffer[2] & 0xF) + ((readBuffer[2] >> 4) & 0x1) * 10 + ((readBuffer[2] >> 0x5) * 20);
+            else
+                hours = (readBuffer[2] & 0xF) + ((readBuffer[2] >> 4) & 0x1) * 10 + ((readBuffer[2] >> 0x5) * 12); ;
+            int day = BcdToInt(readBuffer[3]);
+            int date = BcdToInt(readBuffer[4]);
+            int months = BcdToInt((byte)(readBuffer[5] & (byte)0x3f));
+            int year = BcdToInt(readBuffer[6]);
+            float temperature = (float)BcdToInt(readBuffer[11]) + ((float)(readBuffer[11] >> 0x6)) * 0.25f;
+            return new DateTime(2000 + year, months, date, hours, minutes, seconds);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+                return null;
+            }
+        }
+
+        public float ReadTemperature()
+        {
+            try
+            {
+                byte[] buffer = new byte[2];
+                _device.WriteRead(new byte[] { 0x11 }, buffer);
+                float temperature = (float)buffer[0] + ((float)(buffer[1] >> 6) / 4f);
+                return temperature;
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+                return 0;
+            }
+        }
+
+        public void WriteTime(DateTime dateTime)
+        {
+            byte[] buffer = new byte[8];
+            int offset = 0;
+            buffer[offset++] = 0;
+            buffer[offset++] = IntToBcd(dateTime.Second);
+            buffer[offset++] = IntToBcd(dateTime.Minute);
+            buffer[offset++] = IntToBcd(dateTime.Hour);
+            buffer[offset++] = (byte)dateTime.DayOfWeek;
+            buffer[offset++] = IntToBcd(dateTime.Day);
+            buffer[offset++] = IntToBcd(dateTime.Month);
+            buffer[offset++] = IntToBcd(dateTime.Year % 100);
+            _device.Write(buffer);
+
+        }
+
+
+        static int BcdToInt(byte bcd)
+        {
+            int retVal = (bcd & 0xF) + ((bcd >> 4) * 10);
+            return retVal;
+        }
+
+        static byte IntToBcd(int v)
+        {
+            var retVal = (byte)((v % 10) | (v / 10) << 0x4);
+            return retVal;
+        }
+    }
+
+
+
+
+
+
+
+
+
     /// <summary>
     ///     Ako imete DS3231 (RTC) dogradjen na Raspberry Pi
     ///     onda koristi ovu klasu da postavis i/ili da citas vrijeme
