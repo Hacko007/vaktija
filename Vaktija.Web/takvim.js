b25c4ff01435abe3c93cf23519fb7cd883e8329c
Mithat Hackovic <MHACKOV1@volvocars.com>
2019-12-20 17:50:10 +0100

    last change

--- Vaktija.Web/takvim.js
+++ Vaktija.Web/takvim.js
@@ -13,6 +13,20 @@ var Takvim = /** @class */ (function () {
     Takvim.prototype.getSviPraznici = function () { return null; };
     Takvim.prototype.getDrzavniPraznik = function () { throw new Error("Not implemented"); };
     Takvim.prototype.getVjerskiPraznik = function () { throw new Error("Not implemented"); };
+    Takvim.GetSviPraznici = function () {
+        return;
+        {
+            //Ramazan
+            //    new Praznik(Kalendar.Hidzretski, 9, 1, "Prvi dan posta"),
+            //new Praznik(Kalendar.Hidzretski, 9, 16, "Lejletul Bedr"),
+            //    new Praznik(Kalendar.Hidzretski, 9, 20, "Ulaz u i'tikaf"),
+            //    new Praznik(Kalendar.Hidzretski, 9, 26, "Lejletul Kadr"),
+            //    new Praznik(Kalendar.Hidzretski, 10, 1, "Ramazanski bajram - prvi dan"),
+            //    new Praznik(Kalendar.Hidzretski, 10, 2, "Ramazanski bajram - drugi dan"),
+            //    new Praznik(Kalendar.Hidzretski, 10, 3, "Ramazanski bajram - treći dan")
+        }
+        ;
+    };
     return Takvim;
 }());
 exports.Takvim = Takvim;
